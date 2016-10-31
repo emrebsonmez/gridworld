07c8bd9e3d23b861ee7311c504c704d11ed254e2
Emre Sonmez <emresonmez@Emres-MacBook-Pro-2.local>
2016-10-31 01:10:20 -0400

    updated gridworld to four directional search at each step

--- src/GridWorld.java
+++ src/GridWorld.java
@@ -3,7 +3,9 @@ import java.util.HashSet;
 
 public class GridWorld {
     private int[][] board;
-    private HashSet<ArrayList<int[]>> allPaths = new HashSet<ArrayList<int[]>>();
+    private HashSet<ArrayList<int[]>> allPaths = new HashSet<>();
+    private ArrayList<int[]> optimalPath;
+    private int maxReward = -100000;
 
     private ArrayList<int[]> deepCopy(ArrayList<int[]> oldList) {
         ArrayList<int[]> newList = new ArrayList<>();
@@ -11,6 +13,15 @@ public class GridWorld {
             newList.add(coordinate);
         }
         return newList;
+    }
+
+    private boolean deepContains(int[] coordinate, ArrayList<int[]> visited) {
+        for (int[] loc : visited) {
+            if (coordinate[0] == loc[0] && coordinate[1] == loc[1]) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public GridWorld() {
@@ -24,50 +35,92 @@ public class GridWorld {
         this.board = new int[][]{startRow, goalRow1, emptyRow, emptyRow, goalRow2, emptyRow, goalRow3, emptyRow, emptyRow, goalRow4};
     }
 
-    public void printPath(ArrayList<int[]> path) {
+    private int getReward(ArrayList<int[]> path) {
+        int reward = 0;
+        for (int[] coordinate : path) {
+            reward += board[coordinate[0]][coordinate[1]];
+        }
+        return reward;
+    }
+
+    public void printPath(ArrayList<int[]> path, int reward) {
         String pathString = "";
-        int cost = 0;
         for (int[] coordinate : path) {
             String coordinateString = "(" + coordinate[0] + ", " + coordinate[1] + ")";
             if (pathString.length() > 0) {
                 pathString += ", ";
             }
             pathString += coordinateString;
-            cost += board[coordinate[0]][coordinate[1]];
         }
         System.out.println(pathString);
-        System.out.println("Cost: " + cost);
+        System.out.println("Reward: " + reward);
+    }
+
+    // returns true if coordinate in bounds of board
+    // assumes board is square
+    private boolean inBounds(int[] coordinate) {
+        int x = coordinate[0];
+        int y = coordinate[1];
+        boolean xInBounds = (x < board.length && x >= 0);
+        boolean yInBounds = (y < board.length && y >= 0);
+        return xInBounds && yInBounds;
+    }
+
+    private void copyAndVisit(int[] target, ArrayList<int[]> pathToCurrent) {
+        if (inBounds(target)) {
+            if (!deepContains(target, pathToCurrent)) {
+                ArrayList<int[]> pathToCurrentDeepCopy = deepCopy(pathToCurrent);
+                pathToCurrentDeepCopy.add(target);
+                enumeratePolicies(target, pathToCurrentDeepCopy);
+            }
+        }
     }
 
     /**
      * Will do a breadth first search on the grid, add all paths to goal states to allPaths.
      */
-    public void enumeratePolicies(int x, int y, ArrayList<int[]> pathToCurrent, ArrayList<int[]> visited) {
-        int[] current = {x,y};
-        pathToCurrent.add(current);
+    public void enumeratePolicies(int[] coordinate, ArrayList<int[]> pathToCurrent) {
+        int currentReward = getReward(pathToCurrent);
+        ArrayList<int[]> pathToCurrentDeepCopy = deepCopy(pathToCurrent);
 
+        // Don't continue if a more optimal path has already been found.
+        if (currentReward < maxReward) {
+            return;
+        }
+
+        int x = coordinate[0];
+        int y = coordinate[1];
+
+        // If goal is reached, record path and update maxReward.
         if (board[x][y] == 100) {
-            allPaths.add(pathToCurrent);
-            printPath(pathToCurrent);
+            System.out.println("Goal reached.");
+            allPaths.add(pathToCurrentDeepCopy);
+            printPath(pathToCurrentDeepCopy, currentReward);
+            if (currentReward > maxReward) {
+                maxReward = currentReward;
+                optimalPath = pathToCurrentDeepCopy;
+            }
+            return;
         }
 
-        if (x < board.length -1) {
-            ArrayList<int[]> newPath = deepCopy(pathToCurrent);
-            enumeratePolicies(x + 1, y, newPath, visited);
-        }
+        int[] right = {x+1, y};
+        int[] left = {x-1, y};
+        int[] up = {x, y-1};
+        int[] down = {x, y+1};
 
-        if (y < board.length - 1) {
-            ArrayList<int[]> newPath2 = deepCopy(pathToCurrent);
-            enumeratePolicies(x, y + 1, newPath2, visited);
-        }
-
+        copyAndVisit(left, pathToCurrent);
+        copyAndVisit(right, pathToCurrent);
+        copyAndVisit(up, pathToCurrent);
+        copyAndVisit(down, pathToCurrent);
     }
+
 
     public static void main(String[] args) {
         GridWorld gridWorld = new GridWorld();
         ArrayList<int[]> pathToCurrent = new ArrayList<>();
-        ArrayList<int[]> visited = new ArrayList<>();
-        gridWorld.enumeratePolicies(0, 0, pathToCurrent, visited);
+        int[] start = {0,0};
+        pathToCurrent.add(start);
+        gridWorld.enumeratePolicies(start, pathToCurrent);
     }
 
 }
---
+++ src/QLearner.java
@@ -0,0 +1,5 @@
+/**
+ * Created by emresonmez on 10/31/16.
+ */
+public class QLearner {
+}
